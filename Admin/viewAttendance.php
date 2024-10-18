79e41663a11408d63ebd9b02cdecd41f2efc8582
it66070243 <66070243@kmitl.ac.th>
2024-10-18 21:47:05 +0700

    Update View Attendance.php and add Controller

--- Admin/viewAttendance.php
+++ Admin/viewAttendance.php
@@ -1,56 +1,6 @@
-<?php
-class Database { // OOP
-    private $conn;
-
-    public function __construct() {
-        $this->conn = new mysqli("localhost", "username", "password", "database_name");
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
-
-    public function query($sql) {
-        return $this->conn->query($sql);
-    }
-
-    public function close() {
-        $this->conn->close();
-    }
-}
-
-?>
-
-<?php 
-class viewAttendance { // OOP
-    private $db;
-
-    public function __construct($db) {
-        $this->db = $db;
-    }
-
-    public function getAttendance($dateTaken, $classId, $classArmId) {
-        $query = "SELECT tblattendance.Id, tblattendance.status, tblattendance.dateTimeTaken, tblclass.className,
-                  tblclassarms.classArmName, tblsessionterm.sessionName, tblsessionterm.termId, tblterm.termName,
-                  tblstudents.firstName, tblstudents.lastName, tblstudents.otherName, tblstudents.admissionNumber
-                  FROM tblattendance
-                  INNER JOIN tblclass ON tblclass.Id = tblattendance.classId
-                  INNER JOIN tblclassarms ON tblclassarms.Id = tblattendance.classArmId
-                  INNER JOIN tblsessionterm ON tblsessionterm.Id = tblattendance.sessionTermId
-                  INNER JOIN tblterm ON tblterm.Id = tblsessionterm.termId
-                  INNER JOIN tblstudents ON tblstudents.admissionNumber = tblattendance.admissionNo
-                  WHERE tblattendance.dateTimeTaken = '$dateTaken'
-                  AND tblattendance.classId = '$classId'
-                  AND tblattendance.classArmId = '$classArmId'";
-        
-        return $this->db->query($query);
-    }
-}
-
-?>
-
 <?php 
 error_reporting(0);
-include '../Includes/dbcon.php';
+include 'viewAttendanceController.php';
 include '../Includes/session.php';
 
 $db = new Database();
