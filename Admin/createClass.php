c97bb4e1542fabd0cc4a05a709ed76e663ffa3a0
Ibetfinnz <141387456+Ibetfinnz@users.noreply.github.com>
2024-10-19 16:43:31 +0700

    Update createClass.php

--- Admin/createClass.php
+++ Admin/createClass.php
@@ -54,7 +54,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'edit' && isset($_GET['Id'])) 
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Create Class</h1>
+            <h1 class="h3 mb-0 text-gray-800">Create Course</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="./">Home</a></li>
               <li class="breadcrumb-item active" aria-current="page">Create Class</li>
